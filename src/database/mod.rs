43ef966c546fca97324e5fe3b1bf6b96216afc8f
Daniel Choi <daniel.choi@adhocteam.us>
2022-02-14 10:27:26 -0500

    WIP

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,20 +1,26 @@
-use postgres::{Client, Error, NoTls};
+use tokio_postgres::{Client, Error};
 
 use crate::tls::tls_config;
 
-pub(crate) struct Psql {
-    pub pool: Vec<postgres::Client>,
+pub struct Psql {
+    pub pool: Vec<Client>,
     url: String,
 }
 
 impl Psql {
-    pub(crate) fn new(pool_size: usize, url: String) -> Result<Self, Error> {
+    pub async fn new(pool_size: usize, url: String) -> Result<Self, Error> {
         let mut pool = Vec::with_capacity(pool_size);
         let config_tls = tls_config();
         let tls = tokio_postgres_rustls::MakeRustlsConnect::new(config_tls);
-        pool.iter_mut().for_each(|x| {
-            *x = Client::connect(&url, tls.clone()).expect("Could not make connection");
-        });
+        for _ in 0..pool_size {
+            let (client, conn) = tokio_postgres::connect(&url, tls.clone()).await?;
+            pool.push(client);
+            tokio::spawn(async move {
+                if let Err(e) = conn.await {
+                    eprintln!("connection error: {}", e);
+                }
+            });
+        }
         Ok(Psql { pool, url })
     }
 }
@@ -24,10 +30,11 @@ mod test {
 
     use self::super::*;
 
+    // #[tokio::test]
     #[tokio::test]
     async fn test_dbconnection() {
         let pool_size = 2;
-        let conn = Psql::new(pool_size, "...".into()).unwrap();
+        let conn = Psql::new(pool_size, "host=localhost user=postgres password=admin port=5432 sslmode=require".into()).await.unwrap();
         // Check the pool size is correct
         assert_eq!(pool_size, conn.pool.len());
     }
