6a8de6235c056951fd24837ffc08040c28b7e9ff
Daniel Choi <daniel.choi@adhocteam.us>
2022-02-14 21:49:45 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,20 @@ use api::database::Psql;
 async fn main() -> Result<(), Box<dyn Error>> {
     let conf = api::Configuration::new("./configs/api.yml").await?;
     let conn = Psql::new(5, conf.0[0]["db"]["url_host"].as_str().unwrap().to_string()).await?;
-    let add = api::database::models::Customers::new();
 
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
-    conn.set(&add).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
+    conn.set(&api::database::models::Customers::new()).await;
     
     Ok(())
 }
