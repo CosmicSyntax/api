4e660495ad8d5b19a58df4e1b4fce69d133f0efb
Daniel Choi <daniel.choi@adhocteam.us>
2022-02-20 16:33:32 -0500

    WIP

--- db/0000001_init.up.sql
+++ db/0000001_init.up.sql
@@ -1,19 +1,20 @@
 BEGIN;
 
 	CREATE TABLE IF NOT EXISTS public.customers (
-		id integer NOT NULL,
+		id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
 		uuid uuid NOT NULL,
 		created_at timestamp with time zone DEFAULT now() NOT NULL,
-		updated_at timestamp with time zone DEFAULT now() NOT NULL
+		updated_at timestamp with time zone DEFAULT now() NOT NULL,
+		PRIMARY KEY (id)
 	);
 
-	CREATE SEQUENCE IF NOT EXISTS public.customers_id_seq
+	/* CREATE SEQUENCE IF NOT EXISTS public.customers_id_seq
 		START WITH 2
 		INCREMENT BY 2
 		NO MINVALUE
 		NO MAXVALUE
 		CACHE 1;
 
-	ALTER TABLE ONLY public.customers ALTER COLUMN id SET DEFAULT nextval('public.customers_id_seq');
+	ALTER TABLE ONLY public.customers ALTER COLUMN id SET DEFAULT nextval('public.customers_id_seq'); */
 
 COMMIT;
--- src/database/models/mod.rs
+++ src/database/models/mod.rs
@@ -1,4 +1,3 @@
-use chrono::{DateTime, Local};
 use sql_builder::{quote, SqlBuilder};
 use uuid::Uuid;
 
@@ -7,19 +6,16 @@ use crate::database;
 #[derive(Debug)]
 pub struct Customers {
     uuid: Uuid,
-    created_at: DateTime<Local>,
-    updated_at: DateTime<Local>,
+    // created_at: DateTime<Local>, <-- auto generated
+    // updated_at: DateTime<Local>, 
     // id: u32, <-- auto incremented
 }
 
 impl Customers {
     pub fn new() -> Self {
         let uuid = Uuid::new_v4();
-        let time = chrono::Local::now();
         Customers {
             uuid,
-            created_at: time,
-            updated_at: time,
         }
     }
 }
@@ -34,12 +30,8 @@ impl database::DbExec for Customers {
     fn set(&self) -> String {
         let query = SqlBuilder::insert_into("customers")
             .field("uuid")
-            .field("created_at")
-            .field("updated_at")
             .values(&[
                 &quote(self.uuid.to_string()),
-                &quote(self.created_at.to_string()),
-                &quote(self.updated_at.to_string()),
             ])
             .sql();
         query.unwrap()
