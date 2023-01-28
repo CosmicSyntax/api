364c8648cdb53fdc3d26b237e7c5b137ab1e4813
Daniel Choi <daniel.choi@saltlending.com>
2023-01-28 02:08:27 -0500

    asm fix

--- asm/src/lib.rs
+++ asm/src/lib.rs
@@ -22,9 +22,9 @@ pub fn add() -> u32 {
     let _right: u32;
     unsafe {
         asm!(
-            "mov {0:w}, 2147483647",
-            "mov {1:w}, {0:w}",
-            "add {0:w}, 1",
+            "mov {0:r}, 2147483647",
+            "mov {1:r}, {0:r}",
+            "add {0:r}, 1",
             out(reg) left,
             out(reg) _right,
         );
