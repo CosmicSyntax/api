d29c65ade8c0a1cd6d2188e7c1bd13e1025b6109
Daniel Choi <choi@ibm.com>
2023-12-11 22:01:40 -0500

    updating dep... may need to wait on actix-web

--- src/web/tls/mod.rs
+++ src/web/tls/mod.rs
@@ -2,14 +2,12 @@ use std::fs::File;
 use std::io::BufReader;
 
 use rustls::server::ServerConfig;
-use rustls::{Certificate, PrivateKey};
-use rustls_pemfile::{certs, pkcs8_private_keys};
+use rustls_pemfile::{certs, private_key};
 
 pub fn load_certs(cert: &str, key: &str) -> ServerConfig {
 
     // init server config builder with safe defaults
     let config = ServerConfig::builder()
-        .with_safe_defaults()
         .with_no_client_auth();
 
     // load TLS key/cert files
@@ -18,21 +16,16 @@ pub fn load_certs(cert: &str, key: &str) -> ServerConfig {
 
     // convert files to key/cert objects
     let cert_chain = certs(cert_file)
-        .unwrap()
-        .into_iter()
-        .map(Certificate)
-        .collect();
-    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
-        .unwrap()
-        .into_iter()
-        .map(PrivateKey)
-        .collect();
+        .map(|v| v.unwrap())
+        .collect::<Vec<_>>();
+
+    let mut keys = private_key(key_file).unwrap();
 
     // exit if no keys could be parsed
-    if keys.is_empty() {
+    if let Some(key) = keys {
+        config.with_single_cert(cert_chain, key).unwrap()
+    } else {
         eprintln!("Could not locate PKCS 8 private keys.");
         std::process::exit(1);
     }
-
-    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
 }
